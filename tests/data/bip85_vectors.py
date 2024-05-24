04701f6efddc63b2c3ad108cc6c75fe1ec52d934
Aneesh Karve <aneesh.karve@gmail.com>
2024-05-23 17:19:11 -0700

    vector 3 without drng support

--- tests/data/bip85_vectors.py
+++ tests/data/bip85_vectors.py
@@ -11,4 +11,11 @@ EXT_KEY_TO_ENTROPY = [
         "derived_key": "503776919131758bb7de7beb6c0ae24894f4ec042c26032890c29359216e21ba",
         "derived_entropy": "70c6e3e8ebee8dc4c0dbba66076819bb8c09672527c4277ca8729532ad711872218f826919f6b67218adde99018a6df9095ab2b58d803b5b93ec9802085a690e",
     },
+    {
+    "master": "xprv9s21ZrQH143K2LBWUUQRFXhucrQqBpKdRRxNVq2zBqsx8HVqFk2uYo8kmbaLLHRdqtQpUm98uKfu3vca1LqdGhUtyoFnCNkfmXRyPXLjbKb",
+    "path": "m/83696968'/0'/0'",
+    "derived_key": "cca20ccb0e9a90feb0912870c3323b24874b0ca3d8018c4b96d0b97c0e82ded0",
+    "derived_entropy": "efecfbccffea313214232d29e71563d941229afb4338c21f9517c41aaa0d16f00b83d2a09ef747e7a64e8e2bd5a14869e693da66ce94ac2da570ab7ee48618f7",
+    "drng": "b78b1ee6b345eae6836c2d53d33c64cdaf9a696487be81b03e822dc84b3f1cd883d7559e53d175f243e4c349e822a957bbff9224bc5dde9492ef54e8a439f6bc8c7355b87a925a37ee405a7502991111"
+    },
 ]
