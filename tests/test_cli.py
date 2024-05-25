0d08904eb4d5dac44341edaecb95877540885280
Aneesh Karve <aneesh.karve@gmail.com>
2024-05-25 15:51:40 -0700

    more test coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -21,17 +21,68 @@ def runner():
 @pytest.mark.parametrize("language, vectors", VECTORS.items())
 def test_seed_command_to_actual_seed(runner, language, vectors):
     for vector in vectors:
-        _, mnemonic, seed, xprv = vector
+        _, mnemonic, _, xprv = vector
         result = runner.invoke(
             cli, ["seed", "-t", "xprv", "-f", "words", "-i", mnemonic, "-p", "TREZOR"]
         )
         assert result.exit_code == 0
         assert result.output.strip() == xprv
-        break
+
+        diff_pass = runner.invoke(
+            cli, ["seed", "-t", "xprv", "-f", "words", "-i", mnemonic, "-p", "TrEZOR"]
+        )
+        assert diff_pass.exit_code == 0
+        assert diff_pass.output.strip() != xprv
+
+        diff_mnem = runner.invoke(
+            cli,
+            ["seed", "-t", "xprv", "-f", "words", "-i", mnemonic + ".", "-p", "TrEZOR"],
+        )
+        assert diff_mnem.exit_code == 0
+        assert diff_mnem.output.strip() != xprv
+
+
+@pytest.mark.parametrize("language, vectors", VECTORS.items())
+def test_seed_option_sensitivity(runner, language, vectors):
+    """prove that passphrase and mnemonic changes alter xprv (but white space around
+    mnemonic doesn't)"""
+    # make tests faster by only covering one per language
+    for vector in vectors[:1]:
+        _, mnemonic, _, xprv = vector
+
+        diff_pass = runner.invoke(
+            cli, ["seed", "-t", "xprv", "-f", "words", "-i", mnemonic, "-p", "TrEZOR"]
+        )
+        assert diff_pass.exit_code == 0
+        assert diff_pass.output.strip() != xprv
+
+        diff_mnem = runner.invoke(
+            cli,
+            ["seed", "-t", "xprv", "-f", "words", "-i", mnemonic + ".", "-p", "TREZOR"],
+        )
+        assert diff_mnem.exit_code == 0
+        assert diff_mnem.output.strip() != xprv
+
+        space_mnem = runner.invoke(
+            cli,
+            [
+                "seed",
+                "-t",
+                "xprv",
+                "-f",
+                "words",
+                "-i",
+                "  " + mnemonic + "\t\n ",
+                "-p",
+                "TREZOR",
+            ],
+        )
+        assert space_mnem.exit_code == 0
+        assert space_mnem.output.strip() == xprv
 
 
 @pytest.mark.parametrize("n", SEED_N_RANGE_STR)
-def test_seed_command_words(runner, n):
+def test_seed_command_n_words(runner, n):
     result = runner.invoke(cli, ["seed", "-f", "rand", "-t", "words", "-n", n])
     assert result.exit_code == 0
     assert len(result.output.split()) == int(n)
