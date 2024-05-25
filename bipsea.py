f8ad0f44c4781e83db29e2419a033e78c5acab39
Aneesh Karve <aneesh.karve@gmail.com>
2024-05-25 13:17:21 -0700

    black

--- bipsea.py
+++ bipsea.py
@@ -20,6 +20,7 @@ class InputThread(threading.Thread):
     def run(self):
         self.seed = click.get_text_stream("stdin").read().strip()
 
+
 @click.group()
 @click.version_option(version=__version__, prog_name="bipsea")
 def cli():
@@ -52,16 +53,27 @@ def cli():
 @click.option("-p", "--passphrase", default="")
 def seed(from_, input, to, number, passphrase):
     if to not in SEED_TO_VALUES:
-        raise click.BadOptionUsage(option_name="--to", message=f"must be one of {'|'.join(SEED_TO_VALUES)}")
+        raise click.BadOptionUsage(
+            option_name="--to", message=f"must be one of {'|'.join(SEED_TO_VALUES)}"
+        )
     if from_ not in SEED_FROM_VALUES:
-        raise click.BadOptionUsage(option_name="--from", message=f"must be one of {'|'.join(SEED_FROM_VALUES)}")
+        raise click.BadOptionUsage(
+            option_name="--from", message=f"must be one of {'|'.join(SEED_FROM_VALUES)}"
+        )
     if from_ != "rand" and not input:
-        raise click.BadOptionUsage(option_name="--from", message="--input is required unless --from rand")
+        raise click.BadOptionUsage(
+            option_name="--from", message="--input is required unless --from rand"
+        )
     if from_ == "words":
         if number:
-            raise click.BadOptionUsage(option_name="--number", message="omit --number when you specify --from words")
+            raise click.BadOptionUsage(
+                option_name="--number",
+                message="omit --number when you specify --from words",
+            )
         if to == "words":
-            raise click.BadOptionUsage(option_name="--to", message="--from words is redundant with --to words")
+            raise click.BadOptionUsage(
+                option_name="--to", message="--from words is redundant with --to words"
+            )
     number = 24  # set default here so it doesn't trip the above logic
     if to == "words":
         entropy = None
@@ -71,7 +83,9 @@ def seed(from_, input, to, number, passphrase):
             entropy = hashlib.sha256(input.encode("utf-8")).digest()
         elif from_ == "words":
             click.BadParameter("`--from words --to words` is weirdly redundant")
-        number = number if number else 24  # do this here so we don't break callback validation with defaults
+        number = (
+            number if number else 24
+        )  # do this here so we don't break callback validation with defaults
         words = entropy_to_words(int(number), entropy, passphrase)
         output = "\n".join(f"{i+1}) {w}" for i, w in enumerate(words))
     click.echo(output)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,24 +2,29 @@ import pytest
 from click.testing import CliRunner
 from bipsea import cli
 
+
 @pytest.fixture
 def runner():
     return CliRunner()
 
+
 def test_seed_command(runner):
-    result = runner.invoke(cli, ['seed', '-f', 'rand', '-t', 'words'])
+    result = runner.invoke(cli, ["seed", "-f", "rand", "-t", "words"])
     assert result.exit_code == 0
+
 
 def test_bip85_command(runner):
     pass
 
+
 def test_from_and_to_words(runner):
-    result = runner.invoke(cli, ['seed', '--from', 'words', '--to', 'words'])
+    result = runner.invoke(cli, ["seed", "--from", "words", "--to", "words"])
     assert result.exit_code != 0
     assert "--input" in result.output
     assert "--from rand" in result.output
 
+
 def test_from_and_n(runner):
-    result = runner.invoke(cli, ['seed', '--from', 'words', '-n', '45'])
+    result = runner.invoke(cli, ["seed", "--from", "words", "-n", "45"])
     assert result.exit_code != 0
     assert "--number" in result.output
