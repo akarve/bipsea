f7d302f74627f1ddefcbcf0f56be935cf7fdba07
Aneesh Karve <aneesh.karve@gmail.com>
2024-05-31 20:15:14 -0700

    bump version, setup.py with new python's supported

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="Bitcoin BIP85 BIP32 cryptography",
     install_requires=[
