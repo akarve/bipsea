0c621fdbceaa7ceca3926ffba66b0eb87465fcef
Aneesh Karve <aneesh.karve@gmail.com>
2024-06-09 22:47:47 -0700

    no pipes

--- test-readme.sh
+++ test-readme.sh
@@ -10,12 +10,9 @@ poetry run bipsea mnemonic --help
 poetry run bipsea validate --help
 poetry run bipsea xprv --help
 poetry run bipsea derive --help
-poetry run bipsea mnemonic | poetry run bipsea validate | poetry run bipsea xprv | poetry run bipsea derive -a mnemonic -n 12
 poetry run bipsea mnemonic -t jpn -n 15
 poetry run bipsea mnemonic -t eng -n 12 --pretty
-poetry run bipsea mnemonic -t spa -n 12 | poetry run bipsea validate -f spa
-poetry run bipsea mnemonic | poetry run bipsea validate | poetry run bipsea xprv
-poetry run bipsea validate -f free -m "123456123456123456" | poetry run bipsea xprv
+poetry run bipsea validate -f free -m "123456123456123456"
 poetry run bipsea validate -f free -m @"$(cat input.txt)"
 poetry run bipsea validate -m "$MNEMONIC"
 poetry run bipsea xprv -m "$MNEMONIC"
