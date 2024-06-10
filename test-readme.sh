fd364f041e5e9a167668f5d372c858464db2f0ca
Aneesh Karve <aneesh.karve@gmail.com>
2024-06-09 23:42:24 -0700

    is xprv the issue?

--- test-readme.sh
+++ test-readme.sh
@@ -18,11 +18,9 @@ poetry run bipsea xprv -m "$MNEMONIC" | poetry run bipsea derive -a mnemonic -n 
 
 poetry run bipsea mnemonic -t spa -n 12 | poetry run bipsea validate -f spa
 
-#poetry run bipsea mnemonic | poetry run bipsea validate | poetry run bipsea xprv
+poetry run bipsea validate -f free -m "123456123456123456" | poetry run bipsea xprv
 
-# poetry run bipsea validate -f free -m "123456123456123456" | poetry run bipsea xprv
-
-# poetry run bipsea validate -f free -m "$(cat input.txt)"
+poetry run bipsea validate -f free -m "$(cat input.txt)"
 
 # poetry run bipsea validate -m "$MNEMONIC" | poetry run bipsea xprv | poetry run bipsea derive -a base85
 
