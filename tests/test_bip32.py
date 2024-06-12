744425e03c8ffe21f7b625ad0a7b1d1a8f5354af
Aneesh Karve <aneesh.karve@gmail.com>
2024-06-11 18:03:32 -0700

    ckdpub test

--- tests/test_bip32.py
+++ tests/test_bip32.py
@@ -5,8 +5,9 @@ import pytest
 from data.bip32_vectors import INVALID_KEYS, VECTORS
 from ecdsa import SECP256k1, SigningKey
 from ecdsa.ellipticcurve import INFINITY
+from ecdsa.errors import MalformedPointError
 
-from bipsea.bip32 import CKDpriv, to_master_key, validate_private_child_params, validate_public_child_params
+from bipsea.bip32 import CKDpriv, CKDpub, to_master_key, validate_private_child_params, validate_public_child_params
 from bipsea.bip32types import parse_ext_key, validate_prv_str
 from bipsea.bip85 import derive
 from bipsea.util import LOGGER_NAME
@@ -50,6 +51,13 @@ def test_validate_private_params():
 
    validate_private_child_params(SECP256k1.order - 1, 1, 0)
 
+def test_validate_public_params():
+    private_key = SigningKey.generate(curve=SECP256k1)
+    public_key = private_key.get_verifying_key()
+    with pytest.raises(ValueError):
+        validate_public_child_params(SECP256k1.order, public_key, 0)
+        validate_public_child_params(SECP256k1.order, INFINITY, 0)
+    validate_public_child_params(SECP256k1.order - 1, public_key, 0)
 
 
 @pytest.mark.parametrize(
@@ -95,7 +103,43 @@ def test_ckdpriv_invalid_keys(key_str: str, reason: str):
                         depth=bad_key.depth,
                         version=bad_key.version,
                     )
-    # TODO call CKDpriv, CKDpub, N() on these keys
+
+
+@pytest.mark.parametrize(
+    "key_str, reason",
+    INVALID_KEYS,
+    ids=[f"Vector-5-{reason[:32]}-{key[:8]}" for key, reason in INVALID_KEYS],
+)
+def test_ckdpub(key_str: str, reason: str):
+    with pytest.raises(ValueError):
+        parse_ext_key(key_str)
+    if "checksum" in reason:
+        # the checksum case blows up on base58decode_check even if we don't validate
+        pass
+    else:
+        bad_key = parse_ext_key(key_str, validate=False)
+        if bad_key.is_public():
+            passes = any(s in reason for s in ("zero depth",))
+            with pytest.raises((MalformedPointError, ValueError)) if not passes else no_raise():
+                CKDpub(
+                    public_key=bad_key.data,
+                    chain_code=bad_key.chain_code,
+                    child_number=bad_key.child_number,
+                    depth=bad_key.depth,
+                    version=bad_key.version,
+                    finger = bad_key.finger,
+                )
+        else:
+            with pytest.raises((MalformedPointError, ValueError)):
+                CKDpub(
+                    public_key=bad_key.data,
+                    chain_code=bad_key.chain_code,
+                    child_number=bad_key.child_number,
+                    depth=bad_key.depth,
+                    version=bad_key.version,
+                    finger = bad_key.finger,
+                )
+ 
 
 
 @contextlib.contextmanager
