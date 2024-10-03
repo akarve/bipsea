ee64195d7c1597e9b1d4b0623456b7d00edb2e52
Aneesh Karve <akarve@users.noreply.github.com>
2024-10-03 12:51:20 -0700

    Don't call poetry at runtime (#65)

--- src/bipsea/util.py
+++ src/bipsea/util.py
@@ -10,16 +10,12 @@ import warnings
 from collections import Counter
 from typing import List, Sequence
 
-from poetry.factory import Factory
-
-POETRY = Factory().create_poetry()
-
 MIN_REL_ENTROPY = 0.50  # somewhat magic heuristic
 
-LOGGER_NAME = POETRY.package.name
+__app_name__ = "bipsea"
+__version__ = "2.0.1"
 
-__app_name__ = POETRY.package.name
-__version__ = POETRY.package.version
+LOGGER_NAME = __app_name__
 
 ASCII_INPUTS = set(string.printable.lower())
 FORMAT = "utf-8"
